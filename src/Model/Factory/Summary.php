72bd591dd4c0729ce5ea567680ee158f06624b30
Leo Galleguillos <leo.galleguillos@icloud.com>
2017-12-20 23:23:16 -0800

    Build summary entity with thumbnail attribute

--- src/Model/Factory/Summary.php
+++ src/Model/Factory/Summary.php
@@ -2,6 +2,7 @@
 namespace LeoGalleguillos\Summary\Model\Factory;
 
 use ArrayObject;
+use LeoGalleguillos\Image\Model\Entity\Image as ImageEntity;
 use LeoGalleguillos\Summary\Model\Entity\Summary as SummaryEntity;
 use LeoGalleguillos\Summary\Model\Table\Summary as SummaryTable;
 
@@ -19,6 +20,11 @@ class Summary
         $summaryEntity->summaryId = $arrayObject['summary_id'];
         $summaryEntity->title     = $arrayObject['title'];
         $summaryEntity->body      = $arrayObject['body'];
+
+        $summaryEntity->thumbnail = new ImageEntity();
+        $summaryEntity->thumbnail->rootRelativePath = $arrayObject['thumbnail_root_relative_path'];
+        $summaryEntity->thumbnail->width = $arrayObject['thumbnail_width'];
+        $summaryEntity->thumbnail->height = $arrayObject['thumbnail_height'];
         return $summaryEntity;
     }
 
--- test/Model/Factory/SummaryTest.php
+++ test/Model/Factory/SummaryTest.php
@@ -2,6 +2,7 @@
 namespace LeoGalleguillos\SummaryTest\Model\Factory;
 
 use ArrayObject;
+use LeoGalleguillos\Image\Model\Entity\Image as ImageEntity;
 use LeoGalleguillos\Summary\Model\Entity\Summary as SummaryEntity;
 use LeoGalleguillos\Summary\Model\Factory\Summary as SummaryFactory;
 use LeoGalleguillos\Summary\Model\Table\Summary as SummaryTable;
@@ -26,11 +27,18 @@ class SummaryTest extends TestCase
             'summary_id' => 123,
             'title'      => 'this is the title',
             'body'       => 'this is the body',
+            'thumbnail_root_relative_path'       => '/path/to/image',
+            'thumbnail_width'       => '200',
+            'thumbnail_height'       => '100',
         ]);
         $summaryEntity            = new SummaryEntity();
         $summaryEntity->summaryId = 123;
         $summaryEntity->title     = 'this is the title';
         $summaryEntity->body      = 'this is the body';
+        $summaryEntity->thumbnail = new ImageEntity();
+        $summaryEntity->thumbnail->rootRelativePath = '/path/to/image';
+        $summaryEntity->thumbnail->width = 200;
+        $summaryEntity->thumbnail->height = 100;
 
         $this->assertEquals(
             $summaryEntity,
@@ -44,12 +52,19 @@ class SummaryTest extends TestCase
             'summary_id' => 456,
             'title'      => 'the title for this summary',
             'body'       => 'the body for this summary',
+            'thumbnail_root_relative_path'       => '/path/to/image',
+            'thumbnail_width'       => '450',
+            'thumbnail_height'       => '250',
         ]);
         $this->summaryTable->method('selectWhereSummaryId')->willReturn($arrayObject);
         $summaryEntity            = new SummaryEntity();
         $summaryEntity->summaryId = 456;
         $summaryEntity->title     = 'the title for this summary';
         $summaryEntity->body      = 'the body for this summary';
+        $summaryEntity->thumbnail = new ImageEntity();
+        $summaryEntity->thumbnail->rootRelativePath = '/path/to/image';
+        $summaryEntity->thumbnail->width = 450;
+        $summaryEntity->thumbnail->height = 250;
 
         $this->assertEquals(
             $summaryEntity,
