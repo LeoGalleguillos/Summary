4b889a673212b0146af8881920ff45b8c40c0bda
Leo Galleguillos <leo.galleguillos@icloud.com>
2017-12-21 13:50:36 -0800

    Fix creation of SummarySlug service

--- src/Module.php
+++ src/Module.php
@@ -55,7 +55,7 @@ class Module
                     );
                 },
                 SummarySlugService::class => function ($serviceManager) {
-                    return new SummaryService(
+                    return new SummarySlugService(
                         $serviceManager->get(UrlFriendlyService::class)
                     );
                 },
