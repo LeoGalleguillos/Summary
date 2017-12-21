4c38f49e67c6ece494482e1197578990ae6bbae3
Leo Galleguillos <leo.galleguillos@icloud.com>
2017-12-20 21:36:32 -0800

    Add path, width, and height columns to summary table

--- sql/leogalle_test/summary/create.sql
+++ sql/leogalle_test/summary/create.sql
@@ -2,5 +2,8 @@ CREATE TABLE `summary` (
     `summary_id` int(10) unsigned auto_increment,
     `title` varchar(255) not null,
     `body` text not null,
+    `thumbnail_root_relative_path` varchar(255) default null,
+    `thumbnail_width` int(10) default null,
+    `thumbnail_height` int(10) default null,
     PRIMARY KEY (`summary_id`)
 ) charset=utf8;
