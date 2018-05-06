8b29a67a63c7b6604856df1048ad96a5430f1a17
Leo Galleguillos <leo.galleguillos@icloud.com>
2018-05-06 19:19:27 +0000

    Add n_grams_updated datetime column

--- sql/leogalle_test/summary/create.sql
+++ sql/leogalle_test/summary/create.sql
@@ -2,5 +2,6 @@ CREATE TABLE `summary` (
     `summary_id` int(10) unsigned not null auto_increment,
     `webpage_id` int(10) unsigned not null,
     `title` VARCHAR(255) NOT NULL,
+    `n_grams_updated` DATETIME DEFAULT NULL,
     PRIMARY KEY (`summary_id`)
 ) charset=utf8;
