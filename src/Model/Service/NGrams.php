92de9cee8968127b1728469af3b32fae716ee345
Leo Galleguillos <leo.galleguillos@icloud.com>
2018-05-07 17:19:13 +0000

    Write method to build n grams from tables

--- src/Model/Service/NGrams.php
+++ src/Model/Service/NGrams.php
@@ -44,5 +44,59 @@ class NGrams
         SummaryEntity\Summary $summaryEntity
     ) : array {
         $nGrams = [];
+        $nGrams[2] = [];
+        $nGrams[3] = [];
+        $nGrams[4] = [];
+
+        $generator = $this->nGram2Table->selectWhereSummaryId(
+            $summaryEntity->getSummaryId()
+        );
+        foreach ($generator as $array) {
+            $count = $array['count'];
+            $sequence = [
+                $array['word_1'],
+                $array['word_2'],
+            ];
+            $key = implode(' ', $sequence);
+            $ngrams[2][$key] = [
+                'count' => $count,
+                'sequence' => $sequence,
+            ];
+        }
+
+        $generator = $this->nGram3Table->selectWhereSummaryId(
+            $summaryEntity->getSummaryId()
+        );
+        foreach ($generator as $array) {
+            $count = $array['count'];
+            $sequence = [
+                $array['word_1'],
+                $array['word_2'],
+                $array['word_3'],
+            ];
+            $key = implode(' ', $sequence);
+            $ngrams[3][$key] = [
+                'count' => $count,
+                'sequence' => $sequence,
+            ];
+        }
+
+        $generator = $this->nGram4Table->selectWhereSummaryId(
+            $summaryEntity->getSummaryId()
+        );
+        foreach ($generator as $array) {
+            $count = $array['count'];
+            $sequence = [
+                $array['word_1'],
+                $array['word_2'],
+                $array['word_3'],
+                $array['word_4'],
+            ];
+            $key = implode(' ', $sequence);
+            $ngrams[4][$key] = [
+                'count' => $count,
+                'sequence' => $sequence,
+            ];
+        }
     }
 }
