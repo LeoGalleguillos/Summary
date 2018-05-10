71b0bb4b79a9aa7a131ed1c388d10fa61f4bfd56
Leo Galleguillos <leo.galleguillos@icloud.com>
2018-05-10 15:12:01 +0000

    Update title to be default null

--- sql/leogalle_test/summary/create.sql
+++ sql/leogalle_test/summary/create.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `summary` (
     `summary_id` int(10) unsigned not null auto_increment,
     `webpage_id` int(10) unsigned not null,
-    `title` VARCHAR(255) NOT NULL,
+    `title` VARCHAR(255) DEFAULT NULL,
     `n_grams_updated` DATETIME DEFAULT NULL,
     PRIMARY KEY (`summary_id`)
 ) charset=utf8;
